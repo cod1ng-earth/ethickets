3d224813d04f81616614b76d68d9724f00892a56
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-08-09 10:53:19 +0200

    cors

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
